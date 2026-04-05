70d73803cd2859557b97111eaa41454217983349
MD Waliullah <waliullah.godev@gmail.com>
2026-04-05 15:56:01 +0600

    type casting

--- input.py
+++ input.py
@@ -1,4 +1,4 @@
 first_time = input('Give me first time money: ')
 second_time = input('Give me second time money: ')
 
-print('Total Money: ', first_time + second_time)
+print('Total Money: ', int(first_time) + int(second_time))
