0b2af9d54c864cdc0bec0d9fe229b0a9f700c3ea
MD Waliullah <waliullah.godev@gmail.com>
2026-04-06 08:27:16 +0600

    take paratar in serial wise

--- default-parameters-and-args.py
+++ default-parameters-and-args.py
@@ -13,3 +13,19 @@ print(total)
 def a_lot_of(*args):
     return (args)
 
+
+def full_name(first_name, last_name):
+    return f'{first_name} {last_name}'
+
+
+# take paratar in serial wise
+name = full_name('W', 'H')
+
+# non serial
+name2 = full_name(last_name='W', first_name='H')
+
+print(name)
+
+
+
+
