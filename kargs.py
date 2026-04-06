f61bddb03e63f30b1b1d367701ee530240e5f88b
MD Waliullah <waliullah.godev@gmail.com>
2026-04-06 11:57:49 +0600

    multiple return

--- kargs.py
+++ kargs.py
@@ -13,3 +13,14 @@ def famous_name(fisrt, last, **adition):
 name = famous_name(fisrt='Taheri', last='Ali', title='Huzur', kname='shayakh')
 
 print(name)
+
+
+def a_lot(n, m):
+    sum = n + m;
+    mult = n * m;
+    div = n / m;
+    # return [sum, mult, div];
+    return sum, mult, div;
+
+everything = a_lot(10, 20)
+print(everything)
