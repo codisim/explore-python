c7c90e18d23bdeff34697a78abdc27180a60e72f
MD Waliullah <waliullah.godev@gmail.com>
2026-06-03 06:33:32 +0600

    constructor

--- constructor.py
+++ constructor.py
@@ -1,5 +1,5 @@
 class Phone:
-    manufecturer = "Apple"
+    manufecturer = "Chaina"
 
     def __init__(self, owner, brand, price):
         self.owner = owner
@@ -11,3 +11,12 @@ class Phone:
     def send_message(self, phone, sms):
         text = f"Message: {sms} sent to {phone}"
         return text
+
+
+my_phone = Phone("Kala chan", "Oppo", 8700)
+
+print(my_phone.owner, my_phone.brand, my_phone.price)
+
+
+her_phone = Phone("She amr jaan", "Vivo", 18000)
+print(her_phone.owner, her_phone.brand, her_phone.price)
