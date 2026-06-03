6dd096bdab140e60ad638365f4d3acd15014e43a
MD Waliullah <waliullah.godev@gmail.com>
2026-06-03 08:23:18 +0600

    school

--- student.py
+++ student.py
@@ -25,6 +25,7 @@ class School:
     def __init__(self, name):
         self.name = name
         self.teachers = []
+        self.students = []
 
     def add_student(self, name, subject):
         id = len(self.teachers) + 101
@@ -32,9 +33,41 @@ class School:
         self.teachers.append(teacher)
 
 
+    def enroll(self, fee, name):
+        if fee < 6500:
+            return 'Not enough fee'
+        else: 
+            id = len(self.students) + 1
+            student = Student(name, 11, id)
+            self.students.append(student)
+            return f'{name} is enrolled with {id} ID, extra money: {fee - 6500}'
+        
+    
+    def __repr__(self):
+        print('Welcom to', self.name)
+        print('--------- OUR TEACHERS ---------')
 
-khan = Student('Imran', 11, 11002)
-mia = Teacher(101, 'Nah Khan', 'DSA')
+        print('--------- OUR STUDENTS ---------')
+        for stu in self.students:
+            print(stu)
 
-print(khan)
-print(mia)
+
+
+
+# khan = Student('Imran', 11, 11002)
+# mia = Teacher(101, 'Nah Khan', 'DSA')
+
+# print(khan)
+# print(mia)
+
+
+phitron = School('Phitron')
+phitron.enroll(56000, "kabir")
+phitron.enroll(1200, "Naila")
+phitron.enroll(5000, "Khan")
+
+phitron.add_student('Hanid', "DS")
+phitron.add_student('nia', "OK")
+
+
+print(phitron)
